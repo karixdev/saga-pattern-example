089db131cd7159c17614ab12fd970dd76d2c48e4
karixdev <kari.klucz@gmail.com>
2023-10-31 23:41:41 +0100

    feat: add logging for warehouse service consumer

--- warehouse-service/src/main/java/com/github/karixdev/warehouseservice/consumer/WarehouseInputEventConsumer.java
+++ warehouse-service/src/main/java/com/github/karixdev/warehouseservice/consumer/WarehouseInputEventConsumer.java
@@ -3,10 +3,12 @@ package com.github.karixdev.warehouseservice.consumer;
 import com.github.karixdev.common.event.warehouse.WarehouseInputEvent;
 import com.github.karixdev.warehouseservice.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WarehouseInputEventConsumer {
@@ -15,6 +17,7 @@ public class WarehouseInputEventConsumer {
 
     @KafkaListener(topics = "${topics.warehouse.input}", groupId = "warehouse-service")
     public void consumeWarehouseInputEvent(ConsumerRecord<String, WarehouseInputEvent> record) {
+        log.info("Consumed warehouse input event: {}", record);
         itemService.handleWarehouseInputEvent(record);
     }
 
